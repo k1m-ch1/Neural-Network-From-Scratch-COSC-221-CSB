475e8d70406464cec4e8f89ddaa720de0e027b7d
k1mch1 <kimchourluy@gmail.com>
2026-03-29 00:23:17 +0700

    parsed the labels

--- read_dataset.py
+++ read_dataset.py
@@ -9,26 +9,23 @@ def get_images(filename):
         number_of_rows = int.from_bytes(file.read(4))
         number_of_columns = int.from_bytes(file.read(4))
 
-        #print("Number of images: ", number_of_images)
-        #print("Number of rows: ", number_of_rows)
-        #print("Number of columns: ", number_of_columns)
-
-        #images = []
-        #for _ in range(number_of_images):
-        #    image = []
-        #    for _ in range(number_of_rows):
-        #        row = []
-        #        for _ in range(number_of_columns):
-        #            row.append(int.from_bytes(file.read(1)))
-        #        image.append(row)
-        #    images.append(image)
-        #return images
-
         return [[[int.from_bytes(file.read(1)) for _ in range(number_of_columns)]
             for _ in range(number_of_rows)]
             for _ in range(number_of_images)]
 
+def get_labels(filename):
+    with open(filename, 'rb') as file:
+        magic_bytes = file.read(4)
+        number_of_labels = int.from_bytes(file.read(4))
+        return [int.from_bytes(file.read(1)) for _ in range(number_of_labels)]
+
+
+
 if __name__ == "__main__":
     images = get_images("./dataset/t10k-images.idx3-ubyte")
-    plt.imshow(images[3])
-    plt.show()
+    labels = get_labels("./dataset/t10k-labels.idx1-ubyte")
+    N = 100
+    for n in range(N):
+        plt.title(f"This is an image of a {labels[n]}")
+        plt.imshow(images[n])
+        plt.show()
